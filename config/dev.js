d7ecd4426ea00a96dfea0fa005f7e0095a43e680
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-25 11:54:39 +0700

    setup the db

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://chiefaz:pageraj1@cluster0-shard-00-00-ybeul.mongodb.net:27017,cluster0-shard-00-01-ybeul.mongodb.net:27017,cluster0-shard-00-02-ybeul.mongodb.net:27017/blog_everyone?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
