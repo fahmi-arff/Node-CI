fc1edfc8f3f0adcc5f09a6c5f2797a09a61aea6a
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-25 19:57:10 +0700

    basic redis cache

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -15,7 +15,7 @@ module.exports = app => {
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
     const redis = require('redis');
-    const redisUrl = 'redis://127.0.0.1: 6379';
+    const redisUrl = 'redis://127.0.0.1:6379';
     const client = redis.createClient(redisUrl);
     // utility function
     const util = require('util');
@@ -25,11 +25,17 @@ module.exports = app => {
     const cacheBlogs = await client.get(req.user.id)
 
     // if yes then respond it and return
+    if (cacheBlogs) {
+			console.log('SERVING FROM CACHE')			
+			return res.send(JSON.parse(cacheBlogs));
+		}
 
-
+    // if no, we need to save to cache redis
     const blogs = await Blog.find({ _user: req.user.id });
-
+    console.log('SERVING FROM MONGODB')
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
