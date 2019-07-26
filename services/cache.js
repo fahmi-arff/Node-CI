502996c4fc2afa51b33b0c67627196b90f8397d2
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-26 07:40:12 +0700

    resolving cache value

--- services/cache.js
+++ services/cache.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
 
-const redisUrl = 'redis://127.0.0.0:6379';
+const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
@@ -17,10 +17,15 @@ mongoose.Query.prototype.exec =  async function(){
 
   // If we do, return that
   if(cacheValue){
-    console.log(cacheValue);
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d)): new this.model(doc)
   }
 
   // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
-  console.log(result);
+
+  client.set(key, JSON.stringify(result));
+  return result;
 }
