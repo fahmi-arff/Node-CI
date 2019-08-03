72f8ab4c1d6b200bf69bc488417f34c210aa43ca
Fahmi Arief <fahmi.arff@gmail.com>
2019-08-03 19:15:09 +0700

    add session factory

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const sessionFactory = require('./factories/sessionFactory')
 
 let browser, page;
 
@@ -12,7 +13,7 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
-  // await browser.close();
+  await browser.close();
 })
 
 test('The header has the correct text', async() => {
@@ -30,24 +31,10 @@ test('clicking login starts oauth flow', async() => {
   expect(url).toMatch(/accounts\.google\.com/);
 })
 
-test.only('When signed in, shows logout button', async () => {
-  const id = '5d392975fa6422119845e989';
+test('When signed in, shows logout button', async () => {
+  const { session, sig } = sessionFactory();
 
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  }
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)
-  ).toString('base64')
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
-
-  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session', value: session });
   await page.setCookie({ name: 'session.sig', value: sig });
   await page.goto('localhost:3000');
   await page.waitFor('a[href="/auth/logout"]');
