de04bbb99cfce6ab5811622abffc3bda014b40c7
Fahmi Arief <fahmi.arff@gmail.com>
2019-08-03 14:08:19 +0700

    refactoring before each & add automated close browser

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,13 +1,21 @@
 const puppeteer = require('puppeteer')
 
-test('We can launch a browser', async() => {
-  const browser = await puppeteer.launch({
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   })
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto('localhost:3000');
+})
 
+afterEach(async () => {
+  await browser.close();
+})
+
+test('We can launch a browser', async() => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
   expect(text).toEqual('Blogster');
