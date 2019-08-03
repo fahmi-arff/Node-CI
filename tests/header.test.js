53fb11d870c1aaaa9e676f5b28816f48a339ad9d
Fahmi Arief <fahmi.arff@gmail.com>
2019-08-03 16:11:58 +0700

    generate session and signature

--- tests/header.test.js
+++ tests/header.test.js
@@ -29,3 +29,24 @@ test('clicking login starts oauth flow', async() => {
   // back slash(\) before dot(.) mean we dont use actual . in regex
   expect(url).toMatch(/accounts\.google\.com/);
 })
+
+test('When signed in, shows logout button', async () => {
+  const id = '5d392975fa6422119845e989';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  }
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)
+  ).toString('base64')
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  console.log(sessionString, sig)
+
+})
