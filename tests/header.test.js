5f86b665976fc9de260974cc8b0a12716bf15694
Fahmi Arief <fahmi.arff@gmail.com>
2019-08-03 14:23:56 +0700

    asserting google oauth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -15,8 +15,17 @@ afterEach(async () => {
   await browser.close();
 })
 
-test('We can launch a browser', async() => {
+test('The header has the correct text', async() => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
   expect(text).toEqual('Blogster');
+})
+
+test('clicking login starts oauth flow', async() => {
+  await page.click('.right a');
+
+  const url = await page.url();
+  
+  // back slash(\) before dot(.) mean we dont use actual . in regex
+  expect(url).toMatch(/accounts\.google\.com/);
 })
