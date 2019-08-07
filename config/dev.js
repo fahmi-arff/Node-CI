ac862bd228eaea567c7d8fab8a5799f957efc131
Fahmi Arief <fahmi.arff@gmail.com>
2019-08-07 07:15:30 +0700

    server configuration

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://chiefaz:pageraj1@cluster0-shard-00-00-ybeul.mongodb.net:27017,cluster0-shard-00-01-ybeul.mongodb.net:27017,cluster0-shard-00-02-ybeul.mongodb.net:27017/blog_everyone?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
