090bf98558a12330fcbd24e9291cc590694d5a11
Fahmi Arief <fahmi.arff@gmail.com>
2019-08-08 07:04:07 +0700

    add access key aws

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://chiefaz:pageraj1@cluster0-shard-00-00-ybeul.mongodb.net:27017,cluster0-shard-00-01-ybeul.mongodb.net:27017,cluster0-shard-00-02-ybeul.mongodb.net:27017/blog_everyone?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIATQSZXGIFS6HW2MNJ',
+  secretAccessKey: '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'
 };
